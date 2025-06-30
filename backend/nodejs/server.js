58070926e38525d20f4ecc07170e15f48709c453
krisskariuki <chri570ph3rk4riuki@gmail.com>
2025-06-30 10:10:57 +0300

    disabled default port so such that one must be provided in the  .env file or as an environment variable

--- backend/nodejs/server.js
+++ backend/nodejs/server.js
@@ -1,6 +1,6 @@
 import app from './app.js';
 
-const port = process.env.SERVER_PORT || 5000;
+const port = process.env.SERVER_PORT;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
