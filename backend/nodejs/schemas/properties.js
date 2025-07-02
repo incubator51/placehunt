bcd812e7af2224066ca1222294c687aa156792b3
krisskariuki <chri570ph3rk4riuki@gmail.com>
2025-07-02 20:36:34 +0300

    validation schema for property postings

---
+++ backend/nodejs/schemas/properties.js
@@ -0,0 +1,20 @@
+import * as rules from './utils/validators';
+
+export const postPropertySchema = Joi.object({
+  title: rules.title.required(),
+  description: rules.description.required(),
+  price: rules.price.required(),
+  type: rules.property_types.required(),
+  area: rules.area.required(),
+  bedrooms: rules.bedrooms.optional(),
+  bathrooms: rules.bathrooms.optional(),
+  images: rules.imageArray.required(),
+  location: Joi.object({
+    address: rules.address.required(),
+    coordinates: Joi.object({
+      latitude: rules.latitude.optional(),
+      longitude: rules.longitude.optional(),
+    }).optional(),
+  }).required(),
+  purpose: rules.listing_purpose.required(),
+});
