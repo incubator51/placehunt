5c598e39cbe0998a56a1a4dfab3d65808a0a39a6
krisskariuki <chri570ph3rk4riuki@gmail.com>
2025-07-02 20:34:59 +0300

    moved Joi rules to ./utils/validators.js file for abstraction and reusabilty

--- backend/nodejs/schemas/accounts.js
+++ backend/nodejs/schemas/accounts.js
@@ -1,32 +1,26 @@
-import Joi from 'joi';
+import * as rules from './utils/validators';
 
 export const getAccountsSchema = Joi.object({
-  password: Joi.string().min(8).max(64).pattern(/^\S+$/).required(),
+  password: rules.password,
 });
 export const signupAccountSchema = Joi.object({
-  role: Joi.string().valid('user', 'agent', 'admin').required(),
-  username: Joi.string().min(4).max(32).pattern(/^\S+$/).required(),
-  gender: Joi.string().valid('male', 'female', 'non-binary').required(),
-  email: Joi.string().min(4).max(254).email().pattern(/^\S+$/).required(),
-  password: Joi.string().min(8).max(64).pattern(/^\S+$/).required(),
-  phone: Joi.string()
-    .pattern(/^(\+254|0)(1|7)[0-9]{8}$/)
-    .required(),
-  birth_date: Joi.date()
-    .iso()
-    .min(new Date(new Date().setFullYear(new Date().getFullYear() - 82)))
-    .max(new Date(new Date().setFullYear(new Date().getFullYear() - 18)))
-    .required(),
+  role: rules.role,
+  username: rules.username,
+  gender: rules.gender,
+  email: rules.email,
+  password: rules.password,
+  phone: rules.phone,
+  birth_date: rules.birth_date,
 });
 
 export const signinAccountSchema = Joi.object({
-  username: Joi.string().min(4).max(32).pattern(/^\S+$/),
-  email: Joi.string().min(4).max(254).email().pattern(/^\S+$/),
-  password: Joi.string().min(8).max(64).pattern(/^\S+$/).required(),
+  username: rules.username,
+  email: rules.email,
+  password: rules.password,
 }).xor('username', 'email');
 
 export const deleteAccountSchema = Joi.object({
-  username: Joi.string().min(4).max(32).pattern(/^\S+$/),
-  email: Joi.string().email().pattern(/^\S+$/),
-  password: Joi.string().pattern(/^\S+$/).required(),
+  username: rules.username,
+  email: rules.email,
+  password: rules.password,
 }).xor('username', 'email');
